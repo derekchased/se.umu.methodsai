2e60b8d055ff524d3696c8ccaca620bd70deb399
Ola Ringdahl <ringdahl@therock.cs.umu.se>
2018-11-08 18:11:53 +0100

    Modified the othello bash scripts so that they take a flag for compiling or not as input

--- assignments/Othello/Python/othello.sh
+++ assignments/Othello/Python/othello.sh
@@ -3,14 +3,25 @@
 # Example script for the Othello assignment. Don't forget to make it executable (chmod +x othello)
 # Change the last line (python3 Othello.py ...) if you are using python2 
 #
-# usage: bash othello <position> <time_limit>
+# usage: bash othello <position> <time_limit> <do_compile>
 #
 # Author: Ola Ringdahl
+
+position=$1
+time_limit=$2
+do_compile=$3 
+
+if [ "$#" -ne 3 ]; then
+	# do_compile not set (not enough input arguments) 	 
+	do_compile=0
+fi
 
 # Change directory to the location of your program  
 # $(dirname "$0") is the path to where this script is located (don't change this)
 cd "$(dirname "$0")"
 
-# Call your Python program ($1 and $2 are the input parameters):
-python3 Othello.py $1 $2
-
+# only run if <do_compile> is not set (we don't need to compile Python code, but the automated testing needs this)
+if [ $do_compile -ne 1 ]; then
+	# Call your Python program with a position and time limit
+	python3 Othello.py $position $time_limit
+fi
