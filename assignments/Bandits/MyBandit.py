c192d8b5883dc6c9fab7eb8b9642325df401c984
Derek Yadgaroff <derek@ibelievedigital.com>
2021-01-04 23:47:00 +0100

    ready for handin

--- assignments/Bandits/MyBandit.py
+++ assignments/Bandits/MyBandit.py
@@ -1,4 +1,3 @@
-# epsilon-greedy example implementation of a multi-armed bandit
 import random
 import numpy as np
 import os,sys,inspect
@@ -9,7 +8,7 @@ sys.path.insert(0, parent_dir)
 
 class Bandit:
     """
-    Generic epsilon-greedy bandit that you need to improve
+    Upper confidence bound bandit
     """
 
     def __init__(self, arms, epsilon=0.6):
@@ -17,20 +16,32 @@ class Bandit:
         Initiates the bandits
 
         :param arms: List of arms
-        :param epsilon: Epsilon value for random exploration
+        :param epsilon: The learning rate for UCB
         """
         self.arms = arms
         self.epsilon = epsilon
         self.observations = [[] for x in range(len(arms))]
         self.history = []
-        self.window_size = 20
         self.frequencies = [0] * len(arms)
         self.sums = [0] * len(arms)
 
+
     def N(self):
+        """
+        Calculate the number of runs
+        
+        Returns:
+            int: total number of runs for this bandit object
+        """
         return len(self.history)
 
     def expected_values(self):
+        """
+        The expected value (mean) of each arm
+        
+        Returns:
+            Numpy array: elements correspond by arm index
+        """
         return np.array(self.sums)/np.array(self.frequencies)
 
     def run(self):
@@ -44,21 +55,23 @@ class Bandit:
         if min(self.frequencies) == 0:
             return self.arms[self.frequencies.index(min(self.frequencies))]
         
-        ucb_index = np.argmax(self.expected_values() + self.epsilon * np.sqrt(np.log(self.N())/self.frequencies))
-        #print("ucb",ucb_index,self.expected_values() + c * np.sqrt(np.log(self.N())/self.frequencies))
+        return self.arms[ucb_index()]
 
-        return self.arms[ucb_index]
-
+    def ucb_index(self):
+        """
+        The Upper confidence bound algorithm
         
-        if random.random() < self.epsilon:
-            return self.arms[random.randint(0, len(self.arms) - 1)]
+        Returns:
+            int: the index of the arm based on ucb
+        """
+        return np.argmax(self.expected_values() + self.epsilon * np.sqrt(np.log(self.N())/self.frequencies))
 
-
-        return self.arms[ np.argmax(self.expected_values())  ]
 
     def give_feedback(self, arm, reward):
         """
-        Sets the bandit's reward for the most recent arm pull
+        Sets the bandit's reward for the most recent arm pull.
+        Stores observations for each arm
+
 
         :param arm: The arm that was pulled to generate the reward
         :param reward: The reward that was generated
